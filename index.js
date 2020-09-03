0f14c86422c6f38b4d198573babd53ebb522a410
henry-sev <q2466351926@gmail.com>
2020-09-04 00:27:36 +0800

    dynamic time

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-let p = document.getElementById("time");
+function clock() {
+  let p = document.getElementById("time");
 let nowTime = new Date();
-time.innerText = nowTime.getHours() + ':' + nowTime.getMinutes();
+time.innerText = ("0" + nowTime.getHours()).slice(-2) + 
+  ':' + ("0" + nowTime.getMinutes()).slice(-2) + 
+  ":" + ("0" + nowTime.getSeconds()).slice(-2);
+}
+
+setInterval(function(){clock()}, 1000);
+
+// setInterval(function(){ alert("Hello"); }, 3000);
